d0cd43d1bcb4f25ac8d91ebc8b249d12e8186756
Artem Burashnikov <artem.burashnikov@gmail.com>
2023-11-12 11:33:07 +0300

    style: update docstrings

--- depinspect/files.py
+++ depinspect/files.py
@@ -5,17 +5,6 @@ from pathlib import Path
 def remove_file(file_path: Path) -> None:
     """
     Remove a file at the specified path.
-
-    Parameters:
-    - file_path (Path): The path to the file to be removed.
-
-    Returns:
-    - None
-
-    Note:
-    This function attempts to unlink (delete) the file at the provided path.
-    It logs information if the file is removed successfully or logs exceptions
-    if the file is not found, there's a permission error, or any other OSError occurs.
     """
     try:
         Path.unlink(file_path)
@@ -32,18 +21,8 @@ def list_files_in_directory(directory_path: Path) -> list[Path]:
     """
     List all files in the specified directory.
 
-    Parameters:
-    - directory_path (Path): The path to the directory.
-
     Returns:
     - List[Path]: A list of Path objects representing files in the directory.
-
-    Raises:
-    - NotADirectoryError: If the specified path is not a directory.
-
-    Note:
-    This function checks if the provided path is a directory, lists all files
-    within it, and returns a list of Path objects representing the files.
     """
     if directory_path.is_dir():
         return [path for path in Path.iterdir(directory_path) if path.is_file()]
--- depinspect/helper.py
+++ depinspect/helper.py
@@ -8,9 +8,6 @@ def get_project_root() -> Path:
     """
     Get the absolute path to the root directory of the project.
 
-    Returns:
-    - Path: The absolute path to the root directory of the project.
-
     Note:
     This function uses the absolute path of the current file and navigates
     two levels up to determine the project root directory.
@@ -22,10 +19,6 @@ def create_temp_dir(dir_prefix: str, output_path: Path) -> Path:
     """
     Create a temporary directory with a specified prefix in the given output path.
 
-    Parameters:
-    - dir_prefix (str): The prefix to use for the temporary directory.
-    - output_path (Path): The path where the temporary directory will be created.
-
     Returns:
     - Path: The path to the newly created temporary directory.
     """
@@ -36,16 +29,8 @@ def is_valid_package_name(package_name: str) -> bool:
     """
     Check if a package name is valid.
 
-    Parameters:
-    - package_name (str): The package name to be validated.
-
     Returns:
     - bool: True if the package name is valid, False otherwise.
-
-    Note:
-    This function uses a regular expression pattern to check if the package name
-    follows the specified format: starts with an alphanumeric character, followed
-    by alphanumeric characters or '+', '-', or '.' with a minimum length of 2.
     """
     valid_pattern = fullmatch(r"([a-zA-Z0-9][a-zA-Z0-9+-.]{1,})", package_name)
     return bool(valid_pattern)
@@ -55,15 +40,8 @@ def is_valid_distribution(distribution_name: str) -> bool:
     """
     Check if a distribution name is valid.
 
-    Parameters:
-    - distribution_name (str): The distribution name to be validated.
-
     Returns:
     - bool: True if the distribution name is valid, False otherwise.
-
-    Note:
-    This function checks if the provided distribution name is present in a predefined
-    list of valid distributions.
     """
     # Importing here avoids circular dependency.
     from depinspect.constants import DISTRIBUTIONS
@@ -75,15 +53,8 @@ def is_valid_architecture_name(architecture_name: str) -> bool:
     """
     Check if an architecture name is valid.
 
-    Parameters:
-    - architecture_name (str): The architecture name to be validated.
-
     Returns:
     - bool: True if the architecture name is valid, False otherwise.
-
-    Note:
-    This function checks if the provided architecture name is present in a predefined
-    list of valid architectures.
     """
     # Importing here avoids circular dependency.
     from depinspect.constants import ARCHITECTURES
