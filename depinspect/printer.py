77acaa23bdeebee9189e5d869b75f4ec58a9c972
Artem Burashnikov <artem.burashnikov@gmail.com>
2023-11-12 12:16:07 +0300

    refactor: printer module

--- depinspect/printer.py
+++ depinspect/printer.py
@@ -2,16 +2,16 @@ from click import echo
 
 
 def print_diff(
-    input1: tuple[str, str, str],
-    result_from_input1: list[tuple[str]],
-    input2: tuple[str, str, str],
-    result_from_input2: list[tuple[str]],
+    dist_arch_and_package_name1: tuple[str, str, str],
+    query_result1: list[tuple[str]],
+    dist_arch_and_package_name2: tuple[str, str, str],
+    query_result2: list[tuple[str]],
 ) -> None:
-    def print_result_for_one(
-        package: tuple[str, str, str], result: list[tuple[str]]
+    def print_for_one(
+        dist_arch_and_package_name: tuple[str, str, str], query_result: list[tuple[str]]
     ) -> None:
-        distribution, architecture, package_name = package
-        dependencies = sorted(result[0][0].split(","))
+        distribution, architecture, package_name = dist_arch_and_package_name
+        dependencies = sorted(query_result[0][0].split(","))
         header = f"{distribution} - {architecture} - {package_name}"
 
         max_char_length = (
@@ -27,23 +27,25 @@ def print_diff(
             echo(f"{dependency}")
         echo("\n", nl=False)
 
-    def print_result_for_both(
-        package1: tuple[str, str, str],
-        result1: list[tuple[str]],
-        package2: tuple[str, str, str],
-        result2: list[tuple[str]],
+    def print_for_both(
+        dist_arch_and_package_name1: tuple[str, str, str],
+        query_result1: list[tuple[str]],
+        dist_arch_and_package_name2: tuple[str, str, str],
+        query_result2: list[tuple[str]],
     ) -> None:
-        distribution1, architecture1, package_name1 = package1
-        dependencies1 = sorted(result1[0][0].split(","))
+        distribution1, architecture1, package_name1 = dist_arch_and_package_name1
+        dependencies1 = sorted(query_result1[0][0].split(","))
+        title1 = "These dependencies are present in both:"
         header1 = f"{distribution1} - {architecture1} - {package_name1}"
 
-        distribution2, architecture2, package_name2 = package2
-        dependencies2 = sorted(result2[0][0].split(","))
+        distribution2, architecture2, package_name2 = dist_arch_and_package_name2
+        dependencies2 = sorted(query_result2[0][0].split(","))
+        title2 = "These dependencies are exclusive to:"
         header2 = f"{distribution2} - {architecture2} - {package_name2}"
 
         matches = sorted(set(dependencies1).intersection(dependencies2))
-        differences1 = sorted(set(dependencies1).difference(set(dependencies2)))
-        differences2 = sorted(set(dependencies2).difference(set(dependencies1)))
+        exclusive_to_first = sorted(set(dependencies1).difference(set(dependencies2)))
+        exclusive_to_second = sorted(set(dependencies2).difference(set(dependencies1)))
 
         max_header_length = len(max(header1, header2, key=len))
 
@@ -52,11 +54,11 @@ def print_diff(
         )
 
         diff_max_length1 = max(
-            len(max(differences1, key=len, default="")), max_header_length
+            len(max(exclusive_to_first, key=len, default="")), max_header_length
         )
 
         diff_max_length2 = max(
-            len(max(differences2, key=len, default="")), max_header_length
+            len(max(exclusive_to_second, key=len, default="")), max_header_length
         )
 
         max_length = max(
@@ -64,13 +66,14 @@ def print_diff(
             match_max_length,
             diff_max_length1,
             diff_max_length2,
-            len("These dependencies are present in both"),
+            len(title1),
+            len(title2),
         )
 
         divider = "=" * max_length
 
         echo("\n", nl=False)
-        echo("These dependencies are present in both:")
+        echo(title1)
         echo(header1)
         echo(header2)
         echo(divider)
@@ -78,35 +81,44 @@ def print_diff(
             echo(match)
         echo("\n", nl=False)
 
-        echo("These dependencies are exclusive to:")
+        echo(title2)
         echo(header1)
         echo(divider)
-        for difference in differences1:
-            echo(difference)
+        for package_with_dependencies in exclusive_to_first:
+            echo(package_with_dependencies)
         echo("\n", nl=False)
 
-        echo("These dependencies are exclusive to:")
+        echo(title2)
         echo(header2)
         echo(divider)
-        for difference in differences2:
-            echo(f"{difference}")
+        for package_with_dependencies in exclusive_to_second:
+            echo(f"{package_with_dependencies}")
         echo("\n", nl=False)
 
-    if not result_from_input1 and not result_from_input2:
+    if not query_result1 and not query_result2:
         echo(
-            f"No records were found. Printing input...\n"
-            f"{input1[0]} - {input1[1]} - {input1[2]}\n"
-            f"{input2[0]} - {input2[1]} - {input2[2]}"
+            f"No records were found in the database for\n"
+            f"{dist_arch_and_package_name1[0]} - "
+            f"{dist_arch_and_package_name1[1]} - "
+            f"{dist_arch_and_package_name1[2]}\n"
+            f"{dist_arch_and_package_name2[0]} - "
+            f"{dist_arch_and_package_name2[1]} - "
+            f"{dist_arch_and_package_name2[2]}"
         )
 
-    if not result_from_input1 and result_from_input2:
-        print_result_for_one(input2, result_from_input2)
+    if not query_result1 and query_result2:
+        print_for_one(dist_arch_and_package_name2, query_result1)
 
-    if result_from_input1 and not result_from_input2:
-        print_result_for_one(input1, result_from_input1)
+    if query_result1 and not query_result2:
+        print_for_one(dist_arch_and_package_name1, query_result1)
 
-    if result_from_input1 and result_from_input2:
-        print_result_for_both(input1, result_from_input1, input2, result_from_input2)
+    if query_result1 and query_result2:
+        print_for_both(
+            dist_arch_and_package_name1,
+            query_result1,
+            dist_arch_and_package_name2,
+            query_result2,
+        )
 
 
 def print_list(data: dict[str, list[str]]) -> None:
@@ -119,30 +131,29 @@ def print_list(data: dict[str, list[str]]) -> None:
 
 def print_divergent(
     dist_and_arch1: tuple[str, str],
-    result1: dict[str, list[str]],
+    query_result1: dict[str, list[str]],
     dist_and_arch2: tuple[str, str],
-    result2: dict[str, list[str]],
+    query_result2: dict[str, list[str]],
 ) -> None:
-    result: list[str] = []
+    set_of_packages1 = set(query_result1.keys())
+    set_of_packages2 = set(query_result2.keys())
 
-    set_of_packages1 = set(result1.keys())
-    set_of_packages2 = set(result2.keys())
+    definitely_divergent = set.symmetric_difference(set_of_packages1, set_of_packages2)
+    maybe_divergent = set_of_packages1.intersection(set_of_packages2)
 
-    definitely_diverge = set.symmetric_difference(set_of_packages1, set_of_packages2)
-    maybe_diverge = set_of_packages1.intersection(set_of_packages2)
+    divergent_packages: list[str] = []
+    for package_name in maybe_divergent:
+        if sorted(query_result1[package_name]) != sorted(query_result2[package_name]):
+            divergent_packages.append(package_name)
 
-    for package_name in maybe_diverge:
-        if sorted(result1[package_name]) != sorted(result2[package_name]):
-            result.append(package_name)
-
-    result.extend(definitely_diverge)
+    divergent_packages.extend(definitely_divergent)
 
     echo(
         f"# The following packages have divergent dependencies "
         f"in {dist_and_arch1} and {dist_and_arch2}"
     )
 
-    for item in sorted(result):
-        echo(item)
+    for package_name in divergent_packages:
+        echo(package_name)
 
     echo("\n", nl=False)
