521c5decd260fb401f013d1eedb612b5c6a11406
Artem Burashnikov <artem.burashnikov@gmail.com>
2023-12-06 22:24:45 +0300

    tests: refactor test_fetch.py

--- tests/archives/test_fetch.py
+++ tests/archives/test_fetch.py
@@ -1,31 +1,33 @@
-from urllib import error, request
+from typing import Any
+from urllib import request
 
-from click import echo
+import pytest
 
 from depinspect.constants import PYPROJECT_TOML
 
 
-# Check that all urls defined in toml configuration are reachable
-def test_url_sources() -> None:
-    data = PYPROJECT_TOML["tool"]["depinspect"]["archives"]
+# Define a fixture to get the URLs from the configuration
+@pytest.fixture
+def urls_from_config() -> Any:
+    return PYPROJECT_TOML.get("tool", {}).get("depinspect", {}).get("archives", {})
 
-    for distribution in data.keys():
-        for release in data[distribution].keys():
-            for branch in data[distribution][release].keys():
-                for architecture in data[distribution][release][branch].keys():
-                    url = data[distribution][release][branch][architecture]
 
-                    try:
-                        with request.urlopen(
-                            request.Request(url), timeout=15.0
-                        ) as response:
-                            # Check if the response status code is success
-                            assert (
-                                200 <= response.status < 300
-                            ), f"Unexpected status code for {url}: {response.status}"
-                    except error.URLError as e:
-                        echo(f"Error accessing URL {url}: {e}")
-                        assert False, f"Error accessing URL {url}: {e}"
-                    except Exception as e:
-                        echo(f"Unexpected error for URL {url}: {e}")
-                        assert False, f"Unexpected error for URL {url}: {e}"
+def check_url(url: str) -> None:
+    try:
+        with request.urlopen(request.Request(url), timeout=15.0) as response:
+            # Check if the response status code is success
+            assert (
+                200 <= response.status < 300
+            ), f"Unexpected status code for {url}: {response.status}"
+    except Exception as e:
+        assert False, f"Unexpected error for URL {url}: {e}"
+
+
+def test_url_sources(
+    urls_from_config: dict[str, dict[str, dict[str, dict[str, str]]]]
+) -> None:
+    for _, releases in urls_from_config.items():
+        for _, branches in releases.items():
+            for _, architectures in branches.items():
+                for _, url in architectures.items():
+                    check_url(url)
