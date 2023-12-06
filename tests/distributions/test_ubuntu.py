92151b5427736bd6b8d270c247dbad47e896d351
Artem Burashnikov <artem.burashnikov@gmail.com>
2023-12-06 22:09:38 +0300

    style: format; add type annotation

--- tests/distributions/test_ubuntu.py
+++ tests/distributions/test_ubuntu.py
@@ -1,21 +1,30 @@
 from depinspect.constants import ROOT_DIR
-
 from depinspect.distributions.ubuntu import Ubuntu
 from depinspect.files import list_files_in_directory
 
-def test_ubuntu_parsing():
+
+def test_ubuntu_parsing() -> None:
     # Get a list of Ubuntu package metadata files in the test_data directory
-    ubuntu_packages = [file for file in list_files_in_directory(ROOT_DIR / "tests" / "test_data") if file.name == "test_ubuntu_parsing.txt"]
-    
+    ubuntu_packages = [
+        file
+        for file in list_files_in_directory(ROOT_DIR / "tests" / "test_data")
+        if file.name == "test_ubuntu_parsing.txt"
+    ]
+
     # Deserialize the packages and store them in a dictionary
-    deserialized_packages = {ubuntu_package: Ubuntu.parse_matadata(ubuntu_package) for ubuntu_package in ubuntu_packages}
-    
+    deserialized_packages = {
+        ubuntu_package: Ubuntu.parse_matadata(ubuntu_package)
+        for ubuntu_package in ubuntu_packages
+    }
+
     # Iterate over each metadata file and its corresponding list of packages
     for metadata_file, list_of_packages in deserialized_packages.items():
-        with open(metadata_file, 'r') as f:
+        with open(metadata_file, "r") as f:
             # Read the contents of the metadata file
             metadata = f.read()
 
-            # Iterate over each package entry and assert that it is present in the metadata
+            # Iterate over each package and assert that it is present in the metadata
             for entry in list_of_packages:
-                assert f'Package: {entry.package}\n' in metadata, f"Package {entry.package} not found in {metadata_file}"
+                assert (
+                    f"Package: {entry.package}\n" in metadata
+                ), f"Package {entry.package} not found in {metadata_file}"
