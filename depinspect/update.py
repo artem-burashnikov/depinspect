831d01e84b7bdfae7cb3da80ce68fdc98ca719eb
Artem Burashnikov <artem.burashnikov@gmail.com>
2023-12-07 20:21:34 +0300

    style: rename variables

--- depinspect/update.py
+++ depinspect/update.py
@@ -10,23 +10,23 @@ from depinspect.helper import create_temp_dir
 
 
 def initialize_from_archives(
-    data: dict[str, dict[str, dict[str, dict[str, str]]]],
+    config: dict[str, dict[str, dict[str, dict[str, str]]]],
     db_suffix: str,
     output_path: Path,
 ) -> None:
     tmp_dir = create_temp_dir(dir_prefix=".tmp", output_path=output_path)
 
     try:
-        for distribution in data.keys():
+        for distribution in config.keys():
             logging.info("Fetching archives from pre-defined URL sources.")
-            fetch_and_save_metadata(data, tmp_dir)
+            fetch_and_save_metadata(config, tmp_dir)
 
             logging.info("Extracting archives.")
             process_archives(tmp_dir)
 
             logging.info(f"Processing metadata into {distribution} database.")
-            db_path = database.new(
-                db_name=distribution.join(db_suffix), output_path=output_path
+            db_path = database.init(
+                db_name=f"{distribution}{db_suffix}", output_path=output_path
             )
             deserialize_metadata(tmp_dir, db_path, distribution)
     except Exception:
