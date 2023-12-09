48b3831dd202bd916d67d5cf3c1bccb254f4841e
Artem Burashnikov <artem.burashnikov@gmail.com>
2023-12-09 23:21:52 +0300

    test: fix test_update.py

--- tests/test_update.py
+++ tests/test_update.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import pytest
 
-from depinspect.archives.extract import process_archives
+from depinspect.archives.extract import extract_xz_archive, process_archives
 from depinspect.archives.fetch import fetch_and_save_metadata
 from depinspect.database import database
 from depinspect.distributions.loader import deserialize_metadata
@@ -26,8 +26,8 @@ def data() -> dict[str, dict[str, dict[str, dict[str, str]]]]:
 def test_initialize_from_archives(
     tmp_path: Path, data: dict[str, dict[str, dict[str, dict[str, str]]]]
 ) -> None:
-    fetch_and_save_metadata(data, tmp_path)
-    process_archives(tmp_path)
+    fetch_and_save_metadata(data, "ubuntu", tmp_path)
+    process_archives(tmp_path, tmp_path, ".txt", ".xz", extract_xz_archive)
     db_path = database.init("ubuntu_test.sqlite", tmp_path)
     deserialize_metadata(tmp_path, db_path, "ubuntu", "jammy")
 
