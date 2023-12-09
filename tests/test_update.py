d82f0cea0abc085811f0f1068cbead06f0de9200
Artem Burashnikov <artem.burashnikov@gmail.com>
2023-12-09 23:30:40 +0300

    fix: update test

--- tests/test_update.py
+++ tests/test_update.py
@@ -5,7 +5,7 @@ import pytest
 from depinspect.archives.extract import extract_xz_archive, process_archives
 from depinspect.archives.fetch import fetch_and_save_metadata
 from depinspect.database import database
-from depinspect.distributions.loader import deserialize_metadata
+from depinspect.distributions.loader import deserialize_ubuntu_metadata
 
 
 @pytest.fixture
@@ -29,7 +29,7 @@ def test_initialize_from_archives(
     fetch_and_save_metadata(data, "ubuntu", tmp_path)
     process_archives(tmp_path, tmp_path, ".txt", ".xz", extract_xz_archive)
     db_path = database.init("ubuntu_test.sqlite", tmp_path)
-    deserialize_metadata(tmp_path, db_path, "ubuntu", "jammy")
+    deserialize_ubuntu_metadata(tmp_path, db_path, "ubuntu", "jammy")
 
     assert db_path.is_file(), "Database file is not created"
     assert db_path.suffix == ".sqlite", "Unexpected database file extension"
