1aaedaa2fc7569a1b1092a5b37323f75d4cf7965
Artem Burashnikov <artem.burashnikov@gmail.com>
2023-12-09 15:57:45 +0300

    remove: redundnat default open mode

--- tests/distributions/test_ubuntu.py
+++ tests/distributions/test_ubuntu.py
@@ -19,7 +19,7 @@ def test_ubuntu_parsing() -> None:
 
     # Iterate over each metadata file and its corresponding list of packages
     for metadata_file, list_of_packages in deserialized_packages.items():
-        with open(metadata_file, "r") as f:
+        with open(metadata_file) as f:
             # Read the contents of the metadata file
             metadata = f.read()
 
