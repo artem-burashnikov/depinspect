4f2b6ab03c2d4b532e6a808478021721455a11be
Artem Burashnikov <artem.burashnikov@gmail.com>
2023-12-10 18:06:15 +0300

    add: mapping type annotation

--- depinspect/distributions/mapping.py
+++ depinspect/distributions/mapping.py
@@ -1,4 +1,8 @@
 from depinspect.distributions.fedora import Fedora
+from depinspect.distributions.package import Package
 from depinspect.distributions.ubuntu import Ubuntu
 
-distribution_class_mapping = {"ubuntu": Ubuntu, "fedora": Fedora}
+distribution_class_mapping: dict[str, type[Package]] = {
+    "ubuntu": Ubuntu,
+    "fedora": Fedora,
+}
